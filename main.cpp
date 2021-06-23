530ad7c562057f5ec1c347f1737b3110009d05cc
Mina Ghobrial <mina.ghobrial@elektrobit.com>
2021-06-23 14:18:20 +0300

    separate functionality into functions

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+float addition(float num1, float num2){
+    return num1 + num2;
+}
+
+float division(float num1, float num2){
+    return num1/num2;
+}
+
+void print_result(std::string result_type, float result_value){
+    std::cout << result_type << " result:\t" << result_value << "\n";
+}
+
 int main()
 {
     float first_no, second_no, result_add, result_div;
@@ -10,9 +22,10 @@ int main()
     std::cout << "Enter second number:\t";
     std::cin>> second_no;
 
-    result_add = first_no + second_no;
-    result_div = first_no / second_no;
+    result_add = addition(first_no, second_no);
+    result_div = division(first_no, second_no);
 
-    std::cout<< "Addition result:\t" << result_add<< "\nDivision result:\t" << result_div<< "\n";
+    print_result("Addition", result_add);
+    print_result("Division", result_div);
     return 0;
 }
